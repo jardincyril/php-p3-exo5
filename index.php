ed93404aaee8db4ed5f3f271a6e2523e3e8d8a57
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:53:22 +0200

    boucles

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo5</title>
+  </head>
+  <body>
+    <p>
+    <?php
+
+for ($i = 1; $i <= 15; $i++) {
+   echo 'on y arrive presque. ';
+}
+?>
+</p>
+  </body>
+</html>
